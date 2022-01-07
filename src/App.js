a9b44ae4dd4457898a8645469d72a38385446650
Mostafa Abdo <mostafaabdo@Mostafas-MacBook-Air.local>
2022-01-07 05:02:51 +0200

    using material UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React,{useState,useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import { queryAllByAttribute } from "@testing-library/react";
+
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey='6e3fe13987e045017d59bfee2bf7747a2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
+
+    const [newsArticles , setNewsArticles]= useState([]);
+
     useEffect(()=>{
         alanBtn({
             key:alanKey,
             onCommand: ({command, articles})=>{
                 if(command==='newHeadlines'){
-                    console.log(articles);
+                    setNewsArticles(articles);
                 }
             }
         })
@@ -18,8 +22,9 @@ const App = () =>{
     return(
         <div>
             <h1>Alan AI News Application</h1>
+            <NewsCards articles={newsArticles}/>
         </div>
-    )
+    );
 }
 
 export default App;
