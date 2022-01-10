101c16e541f5bfd9c091a3c3c5eb5e1c982e579e
Mostafa Abdo <mostafaabdo@Mostafas-MacBook-Air.local>
2022-01-10 03:12:53 +0200

    enhancing home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React,{useState,useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-
 import NewsCards from "./components/NewsCards/NewsCards";
-
+import useStyles from "./styles.js";
 const alanKey='6e3fe13987e045017d59bfee2bf7747a2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
 
     const [newsArticles , setNewsArticles]= useState([]);
-
+    const classes= useStyles();
     useEffect(()=>{
         alanBtn({
             key:alanKey,
@@ -21,9 +20,11 @@ const App = () =>{
     },[])
     return(
         <div>
-            <h1>Alan AI News Application</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://46ba123xc93a357lc11tqhds-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/alan.jpg"/>
+            </div>
             <NewsCards articles={newsArticles}/>
-        </div>
+        </div> 
     );
 }
 
