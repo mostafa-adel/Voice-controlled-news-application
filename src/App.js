9244221d71ee6e2677a32fb6b94011d205eb1f41
Mostafa Abdo <mostafaabdo@Mostafas-MacBook-Air.local>
2022-01-15 09:15:58 +0200

    open article by card number

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
 import useStyles from "./styles.js";
+import wordsToNumbers from "words-to-numbers";
+
 const alanKey='6e3fe13987e045017d59bfee2bf7747a2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
@@ -12,13 +14,25 @@ const App = () =>{
     useEffect(()=>{
         alanBtn({
             key:alanKey,
-            onCommand: ({command, articles})=>{
+            onCommand: ({command, articles , number})=>{
                 if(command==='newHeadlines'){
                     setNewsArticles(articles);
                     setSctiveArticle(-1);
                 }
                 else if (command==='highlight'){
                     setSctiveArticle((prevActiveArticle)=>prevActiveArticle +1);
+                }
+                else if (command==='open'){
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number,{fuzzy: true}) : number;
+                    const  article = articles[parsedNumber - 1 ];
+                    if(parsedNumber > 20){
+                        alanBtn().playText('Please try again.');
+                    }
+                    else if(article) {
+                        window.open(article.url,'_blank');
+                        alanBtn().playText('Opening');
+                    }
+                    
                 }
             }
         })
